0187df9a6e675cd452b941b51f591d896f529974
quannhg <quan.nguyenhg12@gmail.com>
2024-04-26 16:18:47 +0700

    feat: change port

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import { envs } from "@configs";
-import { createServer } from "./Server";
+import { envs } from '@configs';
+import { createServer } from './Server';
 
-const PORT = 8080;
+const PORT = 8812;
 
 // DO NOT modify, it is used to resolve port mapping when deploy.
-const HOST = envs.isDev ? "localhost" : "0.0.0.0";
+const HOST = envs.isDev ? 'localhost' : '0.0.0.0';
 
 const app = createServer({
-  host: HOST,
-  port: PORT,
+    host: HOST,
+    port: PORT
 });
 
 app.start();
