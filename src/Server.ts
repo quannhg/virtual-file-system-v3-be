45431a70c746230a83073d0a06d687f1bfb692bb
quannhg <quan.nguyenhg12@gmail.com>
2024-04-27 00:14:21 +0700

    chore: remove redundant files

--- src/Server.ts
+++ src/Server.ts
@@ -1,66 +1,51 @@
-import fastify, { FastifyInstance } from "fastify";
-import type { FastifyCookieOptions } from "@fastify/cookie";
-import { envs, logger, swaggerConfig, swaggerUIConfig } from "@configs";
-import { customErrorHandler } from "@handlers";
-import { apiRoute } from "./routes/api";
+import fastify, { FastifyInstance } from 'fastify';
+import { envs, logger, swaggerConfig, swaggerUIConfig } from '@configs';
+import { customErrorHandler } from '@handlers';
+import { apiRoute } from './routes/api';
 
 export function createServer(config: ServerConfig): FastifyInstance {
-  const app = fastify({
-    logger: envs.isTest ? undefined : logger,
-    disableRequestLogging: !envs.isDev,
-  });
-
-  app.register(import("@fastify/sensible"));
-  app.register(import("@fastify/helmet"));
-  app.register(import("@fastify/cors"), {
-    origin: envs.CORS_WHITE_LIST,
-    credentials: true,
-  });
-
-  app.register(import("@fastify/cookie"), {
-    secret: envs.COOKIE_SECRET, // for cookies signature
-    hook: "onRequest",
-  } as FastifyCookieOptions);
-
-  app.register(import("@fastify/multipart"));
-
-  // Swagger on production will be turned off in the future
-  if (
-    envs.NODE_ENV === "development" ||
-    envs.NODE_ENV === "staging" ||
-    envs.NODE_ENV === "production"
-  ) {
-    app.register(import("@fastify/swagger"), swaggerConfig);
-    app.register(import("@fastify/swagger-ui"), swaggerUIConfig);
-  }
-
-  app.setErrorHandler(customErrorHandler);
-
-  app.register(apiRoute, { prefix: "/api" });
-
-  const start = async () => {
-    await app.listen({
-      host: config.host,
-      port: config.port,
+    const app = fastify({
+        logger: envs.isTest ? undefined : logger,
+        disableRequestLogging: !envs.isDev
     });
-    await app.ready();
-    if (envs.NODE_ENV !== "test") {
-      app.swagger({ yaml: true });
-      logger.info(
-        `Swagger documentation is on http://${config.host}:${config.port}/docs`
-      );
+
+    app.register(import('@fastify/sensible'));
+    app.register(import('@fastify/helmet'));
+    app.register(import('@fastify/cors'), {
+        origin: envs.CORS_WHITE_LIST,
+        credentials: true
+    });
+
+    if (envs.NODE_ENV === 'development' || envs.NODE_ENV === 'staging') {
+        app.register(import('@fastify/swagger'), swaggerConfig);
+        app.register(import('@fastify/swagger-ui'), swaggerUIConfig);
     }
-    process.on("SIGINT", () => {
-      app.log.info("Exited program");
-      process.exit(0);
-    });
-  };
 
-  const shutdown = () => app.close();
+    app.setErrorHandler(customErrorHandler);
 
-  return {
-    ...app,
-    start,
-    shutdown,
-  };
+    app.register(apiRoute, { prefix: '/api' });
+
+    const start = async () => {
+        await app.listen({
+            host: config.host,
+            port: config.port
+        });
+        await app.ready();
+        if (envs.NODE_ENV !== 'test') {
+            app.swagger({ yaml: true });
+            logger.info(`Swagger documentation is on http://${config.host}:${config.port}/docs`);
+        }
+        process.on('SIGINT', () => {
+            app.log.info('Exited program');
+            process.exit(0);
+        });
+    };
+
+    const shutdown = () => app.close();
+
+    return {
+        ...app,
+        start,
+        shutdown
+    };
 }
--- src/interfaces/handler.ts
+++ src/interfaces/handler.ts
@@ -1,21 +1,10 @@
 import { FastifyReply, FastifyRequest, RouteGenericInterface } from 'fastify';
 
 /**
- * Authorized handler
  * @param RS Response data type
  * @param RQ Request data type
  */
 export type Handler<RS = unknown, RQ extends RouteGenericInterface = Record<string, never>> = (
-    req: FastifyRequest<RQ>,
-    res: FastifyReply
-) => Result<RS>;
-
-/**
- * Unauthorized handler
- * @param RS Response data type
- * @param RQ Request data type
- */
-export type RawHandler<RS = unknown, RQ extends RouteGenericInterface = Record<string, never>> = (
     req: FastifyRequest<RQ>,
     res: FastifyReply
 ) => Result<RS>;
